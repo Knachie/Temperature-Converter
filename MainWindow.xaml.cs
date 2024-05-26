94e811651d04dd4a559fd35551ff0a369307a4a9
Kachi Ugwoke <kchugw@gmail.com>
2024-05-26 11:06:27 +0200

    Added functions to convert temperatures to different units. Didn't add a way to do that with the convert button.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,9 +16,49 @@ namespace Temperature_Converter_WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] units = { "Kelvin", "Celcius", "Rankine", "Fahrenheit" };
         public MainWindow()
         {
             InitializeComponent();
+            InputUnit.ItemsSource = units;
+            InputUnit.SelectedIndex = 0;
+
+            OutputUnit.ItemsSource = units;
+            OutputUnit.SelectedIndex = 0;
+
+        }
+
+        private void ConvertButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(InputUnit.SelectedIndex.ToString());
+        }
+
+        private float CelciusToKelvin(float celsius)
+        {
+            float kelvin = celsius + 273.15f;
+            return kelvin;
+        }
+        
+        private float FahrenheitToKelvin(float fahrenheit)
+        {
+            float kelvin = (fahrenheit + 459.67f) / 1.8f;
+            return kelvin;
+        }
+
+        private float RankineToKelvin(float rankine)
+        {
+            float kelvin = rankine / 1.8f;
+            return kelvin;
+        }
+    
+        private float[] KelvinToAll(float kelvin)
+        {
+            float[] result = new float[4];
+            result[0] = kelvin;
+            result[1] = kelvin - 273.15f;
+            result[2] = kelvin * 1.8f;
+            result[3] = result[2] + 459.67f;
+            return result;
         }
     }
 }
