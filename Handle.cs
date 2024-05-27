c98177d6208e901d9f4e67d0723fbe5f220c3ba6
Kachi Ugwoke <kchugw@gmail.com>
2024-05-27 18:21:03 +0200

    Added functionality to button. Haven't made GUI foolproof.

---
+++ Handle.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConvertHandler
+{
+    internal class Handle
+    {
+
+        private static string TryAgain()
+        {
+            Console.WriteLine("Try again");
+            return Console.ReadLine() + "";
+        }
+
+        public static int Int32(string input)
+        {
+            int result = 0;
+            while (true)
+            {
+                try
+                {
+                    result = Convert.ToInt32(input);
+                    break;
+                }
+                catch(OverflowException)
+                {
+                    Console.WriteLine("Number too high");
+                    input = TryAgain();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong format");
+                    input = TryAgain();
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    input = TryAgain();
+                }
+            }
+            return result;
+        }
+
+        public static long Int64(string input)
+        {
+            long result = 0;
+            while (true)
+            {
+                try
+                {
+                    result = Convert.ToInt64(input);
+                    break;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Number too high");
+                    input = TryAgain();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong format");
+                    input = TryAgain();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    input = TryAgain();
+                }
+            }
+            return result;
+        }
+
+        public static short Int16(string input)
+        {
+            short result = 0;
+            while (true)
+            {
+                try
+                {
+                    result = Convert.ToInt16(input);
+                    break;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Number too high");
+                    input = TryAgain();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong format");
+                    input = TryAgain();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    input = TryAgain();
+                }
+            }
+            return result;
+        }
+
+        public static double Double(string input)
+        {
+            double result = 0;
+            while (true)
+            {
+                try
+                {
+                    result = Convert.ToDouble(input);
+                    break;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Number too high");
+                    input = TryAgain();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Wrong format");
+                    input = TryAgain();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex);
+                    input = TryAgain();
+                }
+            }
+            return result;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using ConvertHandler;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -11,54 +12,83 @@ using System.Windows.Shapes;
 
 namespace Temperature_Converter_WPF
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        string[] units = { "Kelvin", "Celcius", "Rankine", "Fahrenheit" };
-        public MainWindow()
-        {
-            InitializeComponent();
-            InputUnit.ItemsSource = units;
-            InputUnit.SelectedIndex = 0;
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		string[] units = { "Kelvin", "Celcius", "Rankine", "Fahrenheit" };
+		public MainWindow()
+		{
+			InitializeComponent();
+			InputUnit.ItemsSource = units;
+			InputUnit.SelectedIndex = 0;
 
-            OutputUnit.ItemsSource = units;
-            OutputUnit.SelectedIndex = 0;
+			OutputUnit.ItemsSource = units;
+			OutputUnit.SelectedIndex = 0;
 
-        }
+		}
 
-        private void ConvertButton_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(InputUnit.SelectedIndex.ToString());
-        }
+		private void ConvertButton_Click(object sender, RoutedEventArgs e)
+		{
+			float input = 0f;
+			float kelvin = 0f;
 
-        private float CelciusToKelvin(float celsius)
-        {
-            float kelvin = celsius + 273.15f;
-            return kelvin;
-        }
-        
-        private float FahrenheitToKelvin(float fahrenheit)
-        {
-            float kelvin = (fahrenheit + 459.67f) / 1.8f;
-            return kelvin;
-        }
+			try
+			{
+				input = (float) Convert.ToDouble(InputText.Text);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show($"Something went wrong. Try again.\n Error code: {ex}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+				input = 0f;
+			}
 
-        private float RankineToKelvin(float rankine)
-        {
-            float kelvin = rankine / 1.8f;
-            return kelvin;
-        }
-    
-        private float[] KelvinToAll(float kelvin)
-        {
-            float[] result = new float[4];
-            result[0] = kelvin;
-            result[1] = kelvin - 273.15f;
-            result[2] = kelvin * 1.8f;
-            result[3] = result[2] + 459.67f;
-            return result;
-        }
-    }
+			switch (InputUnit.SelectedIndex)
+			{
+				case 0:
+					kelvin = input;
+					break;
+				case 1:
+					kelvin = CelciusToKelvin(input);
+					break;
+				case 2:
+					kelvin = RankineToKelvin(input);
+					break;
+				case 3:
+					kelvin = FahrenheitToKelvin(input);
+					break;
+			}
+
+			OutputText.Text = KelvinToAll(kelvin)[OutputUnit.SelectedIndex].ToString();
+		}
+
+		private float CelciusToKelvin(float celsius)
+		{
+			float kelvin = celsius + 273.15f;
+			return kelvin;
+		}
+		
+		private float FahrenheitToKelvin(float fahrenheit)
+		{
+			float kelvin = (fahrenheit + 459.67f) / 1.8f;
+			return kelvin;
+		}
+
+		private float RankineToKelvin(float rankine)
+		{
+			float kelvin = rankine / 1.8f;
+			return kelvin;
+		}
+	
+		private float[] KelvinToAll(float kelvin)
+		{
+			float[] result = new float[4];
+			result[0] = kelvin;
+			result[1] = kelvin - 273.15f;
+			result[2] = kelvin * 1.8f;
+			result[3] = result[2] - 459.67f;
+			return result;
+		}
+	}
 }
