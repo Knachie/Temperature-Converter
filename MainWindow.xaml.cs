a103d6ec350314c5aa9624c522439e1ce5c6c2a4
Kachi Ugwoke <kchugw@gmail.com>
2024-05-27 18:24:47 +0200

    Merged Code-behind into main

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,11 @@
-﻿using ConvertHandler;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Temperature_Converter_WPF
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
 	{
 		string[] units = { "Kelvin", "Celcius", "Rankine", "Fahrenheit" };
 		public MainWindow()
